942a6d59c54c64b4d042591580851f3b4fd9c1eb
manuel.eric.b <manuel.eric.b@gmail.com>
2025-09-28 11:26:09 +0000

    feat: update public

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <link rel="icon" type="image/x-icon" href="/img/seo.ico" />
+        <link rel="icon" type="image/x-icon" href="./img/halal.pngo" />
       </head>
       <body>
         {children}
