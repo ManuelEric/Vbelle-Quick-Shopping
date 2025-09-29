ffb941d8cb83d191202117d6c2ae053a3ab22c9d
manuel.eric.b <manuel.eric.b@gmail.com>
2025-09-29 04:17:12 +0000

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
+import { defineConfig, type Plugin } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlify from "@netlify/vite-plugin-react-router";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
-    tailwindcss(), 
-    reactRouter(), 
+    tailwindcss(),
+    reactRouter(),
     tsconfigPaths(),
-    netlify()
-  ],
+    netlify(),
+    mode === "development"
+      ? {
+          name: "inject-hmr-runtime",
+          apply: "serve",
+        }
+      : null,
+  ].filter(Boolean) as Plugin[],
   server: {
     allowedHosts: [
       "devserver-production--vbelle.netlify.app",
-      "vbelle.store"
-    ]
-  }
-});
+      "vbelle.store",
+    ],
+  },
+}));
